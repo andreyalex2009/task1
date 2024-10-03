8654d44376d22dda663dc02f5d525b1322ba15d5
andreyalex2009 <andreyalex2009@yandex.ru>
2024-10-03 19:01:52 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,10 @@
+# 1 st program
+print(9 ** 0.5 * 5)
+# 2 nd program
+print(9.99 > 9.98 and 1000 != 1000.1)
+# 3 rd program
+print(2 * 2 + 2)
+print(2 * (2 + 2))
+print(2 * 2 + 2 == 2 * (2 + 2))
+# 4 th program
+print(int(float('123.456') * 10) % 10)
